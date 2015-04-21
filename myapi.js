4e49aafb73d62e167125570b53dab18e1af93196
fatkahawai <ceeb@burning-plains.com>
2015-04-22 10:44:08 +1200

    tidied code with jslint

--- myapi.js
+++ myapi.js
@@ -31,36 +31,36 @@ var inputs = [    { pin: '11', gpio: '17', value: 1 },
 // configure Express to serve index.html and any other static pages stored 
 // in the home directory
 //
-  app.configure(function() {
-    app.use(express.favicon());
-    app.use(express['static'](__dirname )); 
-  });
+app.configure(function () {
+  app.use(express.favicon());
+  app.use(express['static'](__dirname));
+});
 
 // ------------------------------------------------------------------------
 // set up Express routes - middleware to handle incoming requests
 //
 
 // Express route for incoming requests for a customer name
-app.get('/inputs/:id', function(req, res){
+app.get('/inputs/:id', function (req, res) {
   // send an object as a JSON string
- console.log('id = '+req.params.id);
- res.send(inputs[req.params.id]);
+  console.log('id = ' + req.params.id);
+  res.send(inputs[req.params.id]);
 }); // apt.get()
 
 // Express route for incoming requests for a list of all inputs
-app.get('/inputs', function(req, res){
+app.get('/inputs', function (req, res) {
   // send an object as a JSON string
   console.log('all inputs');
   res.send(inputs);
 }); // apt.get()
 
 // Express route for any other unrecognised incoming requests
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
   res.send('Unrecognised API call', 404);
 });
 
 // Express route to handle errors
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   if (req.xhr) {
     res.send(500, 'Oops, Something went wrong!');
   } else {
--- myclient.js
+++ myclient.js
@@ -6,18 +6,18 @@
  * loads and executes this code
  */
 
-window.onload = function() {
-  var url, 
-      jqxhr;
-  for( i= 0 ; i < 2 ; i++ ){
-    url = document.URL+'inputs/'+i;       
-    jqxhr = $.getJSON( url, function(data) {
-        console.log('API response received');    
-        $('#input').append('<p>input gpio port '+data['gpio']+' on pin '+data['pin']+' has current value '+data['value']+'</p>');        
-      } 
-    ); 
+window.onload = function () {
+  var url,
+    i,
+    jqxhr;
+  for (i = 0; i < 2; i++) {
+    url = document.URL + 'inputs/' + i;
+    jqxhr = $.getJSON(url, function (data) {
+      console.log('API response received');
+      $('#input').append('<p>input gpio port ' + data['gpio'] + ' on pin ' + data['pin'] + ' has current value ' + data['value'] + '</p>');
+    });
   }
-}; 
+};
 
 
 
