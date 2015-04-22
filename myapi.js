d4dfd43d707afe771908d667cf3eb93ffc33890c
fatkahawai <ceeb@burning-plains.com>
2015-04-22 21:59:53 +1200

    updated for express 4.x

--- myapi.js
+++ myapi.js
@@ -1,7 +1,8 @@
 /**
  * myapi.js
  * 
- * @version 1.0
+ * @version 1.1 - updated for Express 4.x : April 2015
+ *
  * 
  * DESCRIPTION:
  * a "HELLO WORLD" server-side application to demonstrate running a node 
@@ -30,17 +31,9 @@ var inputs = [    { pin: '11', gpio: '17', value: 1 },
 // ------------------------------------------------------------------------
 // configure Express to serve index.html and any other static pages stored 
 // in the home directory
-//
-app.configure(function () {
-  app.use(express.favicon());
-  app.use(express['static'](__dirname));
-});
+app.use(express.static(__dirname));
 
-// ------------------------------------------------------------------------
-// set up Express routes - middleware to handle incoming requests
-//
-
-// Express route for incoming requests for a customer name
+// Express route for incoming requests for a single input
 app.get('/inputs/:id', function (req, res) {
   // send an object as a JSON string
   console.log('id = ' + req.params.id);
@@ -56,13 +49,13 @@ app.get('/inputs', function (req, res) {
 
 // Express route for any other unrecognised incoming requests
 app.get('*', function (req, res) {
-  res.send('Unrecognised API call', 404);
+  res.status(404).send('Unrecognised API call');
 });
 
 // Express route to handle errors
 app.use(function (err, req, res, next) {
   if (req.xhr) {
-    res.send(500, 'Oops, Something went wrong!');
+    res.status(500).send('Oops, Something went wrong!');
   } else {
     next(err);
   }
